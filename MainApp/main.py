57f1ccc1578b54d53d31947a3c0ce68a2761b49e
Arnaud Thiercelin <athiercelin@icloud.com>
2019-03-16 18:18:25 -0700

    Added venv for linux Added calls for I2C Fixed the project in Pycharm to have a run config.

--- MainApp/main.py
+++ MainApp/main.py
@@ -1,9 +1,10 @@
-from tkinter import Tk, Label, Button
+from tkinter import *
+from smbus2 import SMBus
 
-class MainApp:
+class MainController:
     def __init__(self, mainWindow):
         self.mainWindow = mainWindow
-        mainWindow.title("Arno Laser Main Window")
+        mainWindow.title("Arno Laser")
 
 # Full Screening
         pad = 3
@@ -13,64 +14,67 @@ class MainApp:
         # mainWindow.bind('<Escape>', self.toggleFullscreen)
 
         self.version = "0.1"
-        self.tempUnit = "°C"
+        self.tempUnit = "C"
         self.waterFlow = "TBD"
         self.CNCI2CStatus = "TBD"
         self.MCUI2CStatus = "TBD"
+        self.initUI()
+        self.initI2C()
 
+    def initUI(self):
 # Headline
-        self.headlineLabel = Label(mainWindow, text="Arno Laser "+self.version)
+        self.headlineLabel = Label(self.mainWindow, text="Arno Laser "+self.version)
         self.headlineLabel.grid(column=0, row=0, columnspan=4)
 
 # Fans
-        self.fansSwitchLabel = Label(mainWindow, text="Fans:")
+        self.fansSwitchLabel = Label(self.mainWindow, text="Fans:")
         self.fansSwitchLabel.grid(column=0, row=1)
-        self.fansSwitch = Button(mainWindow, text="On/Off", command=self.switchFan)
+        self.fansSwitch = Button(self.mainWindow, text="On/Off", command=self.switchFan)
         self.fansSwitch.grid(column=1, row=1)
 
-        self.fansSpeedLabel = Label(mainWindow, text="Speed: ")
+        self.fansSpeedLabel = Label(self.mainWindow, text="Speed: ")
         self.fansSpeedLabel.grid(column=0, row=2)
-        self.fansSpeedValueLabel = Label(mainWindow, text="TBD")
+        self.fansSpeedValueLabel = Label(self.mainWindow, text="TBD")
         self.fansSpeedValueLabel.grid(column=1, row=2)
 
 # Water
-        self.waterPumpSwitchLabel = Label(mainWindow, text="Water Pump:")
+        self.waterPumpSwitchLabel = Label(self.mainWindow, text="Water Pump:")
         self.waterPumpSwitchLabel.grid(column=2, row=1)
-        self.waterPumpSwitch = Button(mainWindow, text="On/Off", command=self.switchWaterPump)
+        self.waterPumpSwitch = Button(self.mainWindow, text="On/Off", command=self.switchWaterPump)
         self.waterPumpSwitch.grid(column=3, row=1)
 
-        self.waterFlowLabel = Label(mainWindow, text="Water Flow: ")
+        self.waterFlowLabel = Label(self.mainWindow, text="Water Flow: ")
         self.waterFlowLabel.grid(column=2, row=2)
-        self.waterFlowValueLabel = Label(mainWindow, text=self.waterFlow)
+        self.waterFlowValueLabel = Label(self.mainWindow, text=self.waterFlow)
         self.waterFlowValueLabel.grid(column=3, row=2)
 
-        self.waterTempLabel = Label(mainWindow, text="Water Temp: ")
+        self.waterTempLabel = Label(self.mainWindow, text="Water Temp: ")
         self.waterTempLabel.grid(column=2, row=3)
-        self.waterTempValueLabel = Label(mainWindow, text="123"+self.tempUnit)
+        self.waterTempValueLabel = Label(self.mainWindow, text="123"+self.tempUnit)
         self.waterTempValueLabel.grid(column=3, row=3)
 
 # Air Pump
-        self.airPumpSwitchLabel = Label(mainWindow, text="Air Pump:")
+        self.airPumpSwitchLabel = Label(self.mainWindow, text="Air Pump:")
         self.airPumpSwitchLabel.grid(column=0, row=3)
-        self.airPumpSwitch = Button(mainWindow, text="Air Pump:", command=self.switchAirPump)
+        self.airPumpSwitch = Button(self.mainWindow, text="Air Pump:", command=self.switchAirPump)
         self.airPumpSwitch.grid(column=1, row=3)
 
 # MCU + CNC Statuses
-        self.cncI2CStatusLabel = Label(mainWindow, text="CNC: ")
+        self.cncI2CStatusLabel = Label(self.mainWindow, text="CNC: ")
         self.cncI2CStatusLabel.grid(column=0, row=4)
-        self.cncI2CStatusValueLabel = Label(mainWindow, text=self.CNCI2CStatus)
+        self.cncI2CStatusValueLabel = Label(self.mainWindow, text=self.CNCI2CStatus)
         self.cncI2CStatusValueLabel.grid(column=1, row=4)
 
-        self.mcuI2CStatusLabel = Label(mainWindow, text="MCU: ")
+        self.mcuI2CStatusLabel = Label(self.mainWindow, text="MCU: ")
         self.mcuI2CStatusLabel.grid(column=0, row=5)
-        self.mcuI2CStatusValueLabel = Label(mainWindow, text=self.MCUI2CStatus)
+        self.mcuI2CStatusValueLabel = Label(self.mainWindow, text=self.MCUI2CStatus)
         self.mcuI2CStatusValueLabel.grid(column=1, row=5)
 
 #       self.close_button = Button(mainWindow, text="Close", command=mainWindow.quit)
 #       self.close_button.pack()
 
     def toggleFullscreen(self, event):
-        geom = self.mainWindow.winfo_geometry()
+        geom = self.mainWindow.winfo_geometry()  # type: Geometry
         print(geom, self.defaultGeometry)
         self.mainWindow.geometry(self.defaultGeometry)
         self.defaultGeometry = geom
@@ -84,6 +88,21 @@ class MainApp:
     def switchAirPump(self):
         print("switchAirPump")
 
-root = Tk()
-my_gui = MainApp(root)
-root.mainloop()
+    #
+    # I2C
+    #
+    def initI2C(self):
+        try:
+            self.i2cBus = SMBus(1)
+        except:
+            print("Error opening I2C bus")
+
+    def checkI2CStatus(self):
+        bus.read_byte_data(80,0)
+
+    def closeI2C(self):
+        self.i2cBus.close()
+
+mainApp = Tk()
+mainGui = MainController(mainApp)
+mainApp.mainloop()
