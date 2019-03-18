eb0ff946a606b993031c14a8de1a1c0a1c1c1d10
Arnaud Thiercelin <athiercelin@icloud.com>
2019-03-17 21:30:40 -0700

    Added I2C commands to MCU and tested it

--- MainApp/main.py
+++ MainApp/main.py
@@ -1,12 +1,17 @@
+#
+# Arno-Laser MainApp
+# Main.py created on 2019-03-15 by Arnaud Thiercelin
+#
+
 from tkinter import *
-from smbus2 import SMBus
+from smbus import SMBus
 
 class MainController:
     def __init__(self, mainWindow):
         self.mainWindow = mainWindow
         mainWindow.title("Arno Laser")
 
-# Full Screening
+        # Full Screening
         pad = 3
         self.defaultGeometry = mainWindow.geometry
         # mainWindow.geometry("{0}x{1}+0+0".format(
@@ -16,17 +21,24 @@ class MainController:
         self.version = "0.1"
         self.tempUnit = "C"
         self.waterFlow = "TBD"
+        #
+        # States
+        #
         self.CNCI2CStatus = "TBD"
         self.MCUI2CStatus = "TBD"
+        self.isWaterPumpOn = False
+        self.isAirPumpOn = False
+        self.areFansOn = False
+   
         self.initUI()
         self.initI2C()
 
     def initUI(self):
-# Headline
+        # Headline
         self.headlineLabel = Label(self.mainWindow, text="Arno Laser "+self.version)
         self.headlineLabel.grid(column=0, row=0, columnspan=4)
 
-# Fans
+        # Fans
         self.fansSwitchLabel = Label(self.mainWindow, text="Fans:")
         self.fansSwitchLabel.grid(column=0, row=1)
         self.fansSwitch = Button(self.mainWindow, text="On/Off", command=self.switchFan)
@@ -37,7 +49,7 @@ class MainController:
         self.fansSpeedValueLabel = Label(self.mainWindow, text="TBD")
         self.fansSpeedValueLabel.grid(column=1, row=2)
 
-# Water
+        # Water
         self.waterPumpSwitchLabel = Label(self.mainWindow, text="Water Pump:")
         self.waterPumpSwitchLabel.grid(column=2, row=1)
         self.waterPumpSwitch = Button(self.mainWindow, text="On/Off", command=self.switchWaterPump)
@@ -53,13 +65,13 @@ class MainController:
         self.waterTempValueLabel = Label(self.mainWindow, text="123"+self.tempUnit)
         self.waterTempValueLabel.grid(column=3, row=3)
 
-# Air Pump
+        # Air Pump
         self.airPumpSwitchLabel = Label(self.mainWindow, text="Air Pump:")
         self.airPumpSwitchLabel.grid(column=0, row=3)
         self.airPumpSwitch = Button(self.mainWindow, text="Air Pump:", command=self.switchAirPump)
         self.airPumpSwitch.grid(column=1, row=3)
 
-# MCU + CNC Statuses
+        # MCU + CNC Statuses
         self.cncI2CStatusLabel = Label(self.mainWindow, text="CNC: ")
         self.cncI2CStatusLabel.grid(column=0, row=4)
         self.cncI2CStatusValueLabel = Label(self.mainWindow, text=self.CNCI2CStatus)
@@ -73,6 +85,9 @@ class MainController:
 #       self.close_button = Button(mainWindow, text="Close", command=mainWindow.quit)
 #       self.close_button.pack()
 
+#
+# UI Controls Commands
+#
     def toggleFullscreen(self, event):
         geom = self.mainWindow.winfo_geometry()  # type: Geometry
         print(geom, self.defaultGeometry)
@@ -80,26 +95,66 @@ class MainController:
         self.defaultGeometry = geom
 
     def switchFan(self):
-        print("SwitchFan")
-
+        if self.areFansOn:
+            #turn off
+            self.sendToMCU(0x21, 0)
+            self.areFansOn = False
+        else:
+            #turn on
+            self.sendToMCU(0x20, 0)
+            self.areFansOn = True
+            
     def switchWaterPump(self):
-        print("switchWaterPump")
+        if self.isWaterPumpOn:
+            #turn off
+            self.sendToMCU(0x31, 0)
+            self.isWaterPumpOn = False
+        else:
+            #turn on
+            self.sendToMCU(0x30, 0)
+            self.isWaterPumpOn = True
 
     def switchAirPump(self):
-        print("switchAirPump")
+        if self.isAirPumpOn:
+            #turn off
+            self.sendToMCU(0x11, 0)
+            self.isAirPumpOn = False
+        else:
+            #turn On
+            self.sendToMCU(0x10, 0)
+            self.isAirPumpOn = True
 
-    #
-    # I2C
-    #
+#
+# I2C
+#
     def initI2C(self):
         try:
-            self.i2cBus = SMBus(1)
+            self.i2cBus = SMBus(1) # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
         except:
             print("Error opening I2C bus")
+            return
+        self.MCUI2CAddress = 0x8
+        self.CNCI2CAddress = 0x10
 
     def checkI2CStatus(self):
         bus.read_byte_data(80,0)
+        #Write a single register
+        #cmd = 0x32
+        #value = 0x80
+        #bus.write_byte_data(self.MCUI2CAddress, cmd, value)
 
+        #Write an array of registers
+        #values = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
+        #bus.write_i2c_block_data(self.MCUI2CAddress, cmd, values)
+
+    def sendToMCU(self, cmdId, value):
+        self.i2cBus.write_byte_data(self.MCUI2CAddress, cmdId, value)
+        return
+
+    def sendToCNC(self, cmdId, value):
+        self.i2cBus.write_byte_data(self.CNCI2CAddress, cmdId, value)        
+        return
+        
     def closeI2C(self):
         self.i2cBus.close()
 
